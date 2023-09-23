0587b133709075b074ec47e2ed17ae847943b326
Marcel Hernandez <e6990620@gmail.com>
2023-09-23 22:43:00 +0200

    implemented base58 encoding

---
+++ src/Base58.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bitcoin;
+
+final class Base58
+{
+    private const BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
+
+    public static function encode(string $data): string
+    {
+        $whitespace = 0;
+        while ($whitespace < \strlen($data) && "\x00" === $data[$whitespace]) {
+            ++$whitespace;
+        }
+
+        $result = '';
+        $num = gmp_import($data);
+
+        while ($num > 0) {
+            [$num, $mod] = gmp_div_qr($num, 58);
+            $result = self::BASE58_ALPHABET[gmp_intval($mod)].$result;
+        }
+
+        return str_repeat('1', $whitespace).$result;
+    }
+
+    public static function decode(string $data): string
+    {
+        // TODO
+        return '';
+    }
+}
