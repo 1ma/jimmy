894347622fd2a5326f306d24eba53158bd8bba63
Marcel Hernandez <e6990620@gmail.com>
2025-04-18 19:33:19 +0200

    add Trezor test vectors

--- tests/BIP39/MnemonicTest.php
+++ tests/BIP39/MnemonicTest.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Bitcoin\Tests\BIP39;
 
 use Bitcoin\BIP39\Mnemonic;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 final class MnemonicTest extends TestCase
 {
+    private const string TEST_VECTORS_PATH       = __DIR__.'/../../vendor/trezor/bip39/vectors.json';
     private const string REFERENCE_WORDLIST_PATH = __DIR__.'/../../vendor/trezor/bip39/src/mnemonic/wordlist/english.txt';
 
     public function testWordlistIsCorrect(): void
@@ -30,5 +32,20 @@ final class MnemonicTest extends TestCase
         $zeroSeed   = array_fill(0, 11, 'abandon');
         $zeroSeed[] = 'about';
         self::assertSame(hex2bin('00000000000000000000000000000000'), Mnemonic::decode($zeroSeed));
+    }
+
+    #[DataProvider('trezorEnglishVectorsProvider')]
+    public function testTrezorVectors(string $entropy, string $words, string $seed, string $xprv): void
+    {
+        if (12 !== \count(explode(' ', $words))) {
+            $this->markTestSkipped('Currently only 12 word mnemonics are supported');
+        }
+
+        self::assertSame(hex2bin($entropy), Mnemonic::decode(explode(' ', $words)));
+    }
+
+    public static function trezorEnglishVectorsProvider(): array
+    {
+        return json_decode(file_get_contents(self::TEST_VECTORS_PATH))->english;
     }
 }
