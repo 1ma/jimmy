e50fb5589b4ba1af5ebc9b6db3eb7629beda3813
Marcel Hernandez <e6990620@gmail.com>
2025-05-23 21:27:15 +0200

    WIP

--- src/MerkleTree.php
+++ src/MerkleTree.php
@@ -7,6 +7,7 @@ namespace Bitcoin;
 final class MerkleTree
 {
     private readonly int $total;
+    private readonly int $maxDepth;
     private mixed $nodes;
     private int $currentDepth;
     private int $currentIndex;
@@ -16,28 +17,67 @@ final class MerkleTree
         $this->total        = $total;
         $this->currentIndex = 0;
         $this->currentDepth = 0;
-        $maxDepth           = \strlen(decbin($this->total));
+        $this->maxDepth     = \strlen(decbin($this->total));
 
         $this->nodes = [];
-        for ($depth = 1; $depth <= $maxDepth; ++$depth) {
-            $numItems      = $this->total / (2 ** ($maxDepth - $depth));
+        for ($depth = 1; $depth <= $this->maxDepth; ++$depth) {
+            $numItems      = (int) floor($this->total / (2 ** ($this->maxDepth - $depth)));
             $levelHashes   = array_fill(0, $numItems, null);
             $this->nodes[] = $levelHashes;
         }
     }
 
-    public static function fromLeaves(array $leaves): self
+    public function up(): void
     {
-        $tree = new self(\count($leaves));
+        --$this->currentDepth;
+        $this->currentIndex = intdiv($this->currentIndex, 2);
+    }
 
-        $depth = \count($tree->nodes);
-        $tree->setLevel($depth - 1, $leaves);
+    public function left(): void
+    {
+        ++$this->currentDepth;
+        $this->currentIndex *= 2;
+    }
 
-        for ($level = $depth - 2; $level >= 0; --$level) {
-            $tree->setLevel($level, Hashing::merkleParentLevel($tree->getLevel($level + 1)));
-        }
+    public function right(): void
+    {
+        ++$this->currentDepth;
+        $this->currentIndex *= 2 + 1;
+    }
 
-        return $tree;
+    public function root(): ?string
+    {
+        return $this->nodes[0][0];
+    }
+
+    public function setCurrentNode(string $item): void
+    {
+        $this->nodes[$this->currentDepth][$this->currentIndex] = $item;
+    }
+
+    public function getCurrentNode(array $items): ?string
+    {
+        return $this->nodes[$this->currentDepth][$this->currentIndex];
+    }
+
+    public function getLeftNode(): ?string
+    {
+        return $this->nodes[$this->currentDepth + 1][$this->currentIndex * 2];
+    }
+
+    public function getRightNode(): ?string
+    {
+        return $this->nodes[$this->currentDepth + 1][$this->currentIndex * 2 + 1];
+    }
+
+    public function isLeaf(): bool
+    {
+        return $this->currentDepth == $this->maxDepth;
+    }
+
+    public function rightExists(): bool
+    {
+        return \count($this->nodes[$this->currentDepth + 1]) > $this->currentIndex * 2 + 1;
     }
 
     public function getLevel(int $level): array
--- tests/MerkleTreeTest.php
+++ tests/MerkleTreeTest.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace Bitcoin\Tests;
 
+use Bitcoin\Hashing;
 use Bitcoin\MerkleTree;
 use PHPUnit\Framework\TestCase;
 
 final class MerkleTreeTest extends TestCase
 {
-    public function testIt(): void
+    public function testDirectPopulation(): void
     {
         // Based on example from Chapter 11 page 200
-
         $hashes = [
             hex2bin('9745f7173ef14ee4155722d1cbf13304339fd00d900b759c6f9d58579b5765fb'),
             hex2bin('5573c8ede34936c29cdfdfe743f7f5fdfbd4f54ba0705259e62f39917065cb9b'),
@@ -32,7 +32,12 @@ final class MerkleTreeTest extends TestCase
             hex2bin('f9dbfafc3af3400954975da24eb325e326960a25b87fffe23eef3e7ed2fb610e'),
         ];
 
-        $tree = MerkleTree::fromLeaves($hashes);
+        $tree = new MerkleTree(\count($hashes));
+        $tree->setLevel(4, $hashes);
+        $tree->setLevel(3, Hashing::merkleParentLevel($tree->getLevel(4)));
+        $tree->setLevel(2, Hashing::merkleParentLevel($tree->getLevel(3)));
+        $tree->setLevel(1, Hashing::merkleParentLevel($tree->getLevel(2)));
+        $tree->setLevel(0, Hashing::merkleParentLevel($tree->getLevel(1)));
 
         $debugRepresentation = <<<MERKLETREE
 *597c4baf...*
@@ -43,5 +48,57 @@ final class MerkleTreeTest extends TestCase
 MERKLETREE;
 
         self::assertSame($debugRepresentation, (string) $tree);
+    }
+
+    public function testDepthFirstPopulation(): void
+    {
+        // Based on example from Chapter 11 page 203
+        $hashes = [
+            hex2bin('9745f7173ef14ee4155722d1cbf13304339fd00d900b759c6f9d58579b5765fb'),
+            hex2bin('5573c8ede34936c29cdfdfe743f7f5fdfbd4f54ba0705259e62f39917065cb9b'),
+            hex2bin('82a02ecbb6623b4274dfcab82b336dc017a27136e08521091e443e62582e8f05'),
+            hex2bin('507ccae5ed9b340363a0e6d765af148be9cb1c8766ccc922f83e4ae681658308'),
+            hex2bin('a7a4aec28e7162e1e9ef33dfa30f0bc0526e6cf4b11a576f6c5de58593898330'),
+            hex2bin('bb6267664bd833fd9fc82582853ab144fece26b7a8a5bf328f8a059445b59add'),
+            hex2bin('ea6d7ac1ee77fbacee58fc717b990c4fcccf1b19af43103c090f601677fd8836'),
+            hex2bin('457743861de496c429912558a106b810b0507975a49773228aa788df40730d41'),
+            hex2bin('7688029288efc9e9a0011c960a6ed9e5466581abf3e3a6c26ee317461add619a'),
+            hex2bin('b1ae7f15836cb2286cdd4e2c37bf9bb7da0a2846d06867a429f654b2e7f383c9'),
+            hex2bin('9b74f89fa3f93e71ff2c241f32945d877281a6a50a6bf94adac002980aafe5ab'),
+            hex2bin('b3a92b5b255019bdaf754875633c2de9fec2ab03e6b8ce669d07cb5b18804638'),
+            hex2bin('b5c0b915312b9bdaedd2b86aa2d0f8feffc73a2d37668fd9010179261e25e263'),
+            hex2bin('c9d52c5cb1e557b92c84c52e7c4bfbce859408bedffc8a5560fd6e35e10b8800'),
+            hex2bin('c555bc5fc3bc096df0a0c9532f07640bfb76bfe4fc1ace214b8b228a1297a4c2'),
+            hex2bin('f9dbfafc3af3400954975da24eb325e326960a25b87fffe23eef3e7ed2fb610e'),
+            hex2bin('38faf8c811988dff0a7e6080b1771c97bcc0801c64d9068cffb85e6e7aacaf51'),
+        ];
+
+        $this->markTestSkipped('borked code ahead');
+
+        $tree = new MerkleTree(\count($hashes));
+        $tree->setLevel(4, $hashes);
+        while (null === $tree->root()) {
+            if ($tree->isLeaf()) {
+                $tree->up();
+            } else {
+                $leftHash = $tree->getLeftNode();
+                if (null === $leftHash) {
+                    $tree->left();
+                } elseif ($tree->rightExists()) {
+                    $rightHash = $tree->getRightNode();
+                    if (null === $rightHash) {
+                        $tree->right();
+                    } else {
+                        $tree->setCurrentNode(Hashing::merkleParent($leftHash, $rightHash));
+                        $tree->up();
+                    }
+                } else {
+                    $tree->setCurrentNode(Hashing::merkleParent($leftHash, $leftHash));
+                    $tree->up();
+                }
+            }
+        }
+
+        self::assertTrue(true);
     }
 }
